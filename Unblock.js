866242f18d99462a06fdaf5e18a32ec91d4d78b4
demo <43675065+SylarYu@users.noreply.github.com>
2020-06-21 12:37:59 +0800

    Delete Unblock.js

--- Unblock.js
+++
@@ -1,7 +0,0 @@
-/**
-     * @supported CFE5B2O0 AXXXC001
-     */
-    var body = $response.body;
-    body = '\/*\n@supported E9BEF8E72BD8 70F56792F401 56C6C893EC56\n*\/\n' + body;
-    
-    $done(body);
